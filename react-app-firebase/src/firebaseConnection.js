d309812cd2379e3b510409cefd32f2922db8e877
Luis Oliveira <luis.oliveira@thomasgregit.com.br>
2023-12-27 16:11:17 -0300

    git-ignore firebaseConnection

--- react-app-firebase/src/firebaseConnection.js
+++ react-app-firebase/src/firebaseConnection.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "xxxxxxxxx",
-    authDomain: "domain-your.firebaseapp.com",
-    projectId: "domain-your",
-    storageBucket: "domain-your.appspot.com",
-    messagingSenderId: "99999999999",
-    appId: "1:xxxxxxxxxx:web:yyyyyyyy",
-    measurementId: "G-xxxxx"
+    apiKey: "AIzaSyD1FMRHaKR9OoCzoWO1ns9tBPOTCgW_Fls",
+    authDomain: "react-curso-zero.firebaseapp.com",
+    projectId: "react-curso-zero",
+    storageBucket: "react-curso-zero.appspot.com",
+    messagingSenderId: "982485372624",
+    appId: "1:982485372624:web:5297edaf8826b84e3998d5",
+    measurementId: "G-R8T3YC755T"
   };
   
 const firebaseApp = initializeApp(firebaseConfig);
