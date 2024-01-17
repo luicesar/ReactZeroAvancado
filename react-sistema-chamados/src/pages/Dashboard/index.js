3ad0661568562f0802a9ba6aa95ec461e2c33184
Luis Oliveira <luis.oliveira@thomasgregit.com.br>
2024-01-16 22:24:36 -0300

    Chamados - Cadastrar clientes

---
+++ react-sistema-chamados/src/pages/Dashboard/index.js
@@ -0,0 +1,21 @@
+import { useContext } from 'react'
+import {AuthContext} from '../../contexts/auth'
+
+import Header from '../../components/Header'
+
+export default function Dashboard(){
+  const { logout } = useContext(AuthContext);
+
+  async function handleLogout(){
+    await logout();
+  }
+
+  return(
+    <div>
+      <Header/>
+
+      <h1>Pagina Dashboard</h1>
+      <button onClick={handleLogout}>Sair da conta</button>
+    </div>
+  )
+}
